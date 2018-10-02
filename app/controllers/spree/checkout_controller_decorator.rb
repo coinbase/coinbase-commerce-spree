44a418edaea6545c2b4b9410c764d075d67d709e
Alexander Dobrzhansky <odobrzhanskyi@lohika.com>
2018-10-03 02:28:01 +0300

    spree gateway (#1)

---
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -0,0 +1,43 @@
+module Spree
+  class CheckoutController < StoreController
+    before_action :redirect_to_coinbase_commerce, only: :update
+
+    def redirect_to_coinbase_commerce
+      order = current_order || raise(ActiveRecord::RecordNotFound)
+      if params[:state] == "payment"
+        return unless params[:order][:payments_attributes]
+        payment_method = PaymentMethod.find(params[:order][:payments_attributes].first[:payment_method_id])
+
+      elsif params[:state] == "confirm"
+        payment_method = @order.payment_method
+      end
+
+      if !payment_method.nil? && payment_method.kind_of?(PaymentMethod::CoinbaseCommercePayment)
+        client = payment_method.create_client
+        payment = order.payments.create!(amount: order.total,
+                                         payment_method: payment_method,
+                                         order_id: order.id)
+        payment.started_processing!
+        charge_info = {
+            "name": "Order #%s" % order.number,
+            "description": "Spree order",
+            "pricing_type": "fixed_price",
+            "metadata": {
+                "order_id": order.id,
+                "payment_id": payment.id
+            },
+            "local_price": {
+                "amount": order.total,
+                "currency": order.currency
+            },
+            "redirect_url": "http://#{request.host}:#{request.port}/orders/#{order.number}",
+            "requested_info": ["name", "email"]
+        }
+        charge = client.charge.create(charge_info)
+        redirect_to "#{charge.hosted_url}"
+      end
+    end
+  end
+end
+
+
---
+++ lib/spree_coinbase_commerce.rb
@@ -0,0 +1,4 @@
+require 'spree_core'
+require 'spree_extension'
+require 'spree_coinbase_commerce/engine'
+require 'spree_coinbase_commerce/version'
