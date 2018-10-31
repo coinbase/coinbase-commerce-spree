205046c814e89de8120b0891de5db8f9cf17405a
Paul Collier <paul.collier@coinbase.com>
2018-10-31 18:01:34 -0400

    Populate charge name/desc from payment name/desc

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -18,9 +18,11 @@ module Spree
                                          payment_method: payment_method,
                                          order_id: order.id)
         payment.started_processing!
+        name = payment_method.name.truncate(30)
+        description = payment_method.description.truncate(80)
         charge_info = {
-            "name": "Order #%s" % order.number,
-            "description": "Spree order",
+            "name": name.empty? ? "Order #{order.number}" : name,
+            "description": description.empty? ? 'Pay with cryptocurrency' : description,
             "pricing_type": "fixed_price",
             "metadata": {
                 "order_id": order.id,
